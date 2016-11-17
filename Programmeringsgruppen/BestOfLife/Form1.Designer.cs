c91461f6ece1a33cd6262bd07c0c5fd7825b78e8
NogginBops <julius_hager@hotmail.com>
2016-11-17 17:32:42 +0100

    Added Best of life project

---
+++ Programmeringsgruppen/BestOfLife/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace BestOfLife
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.UpdateTimer = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.ChangeColorButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UpdateTimer
+            // 
+            this.UpdateTimer.Tick += new System.EventHandler(this.UpdateTimer_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.LargeChange = 10;
+            this.trackBar1.Location = new System.Drawing.Point(343, 12);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(188, 45);
+            this.trackBar1.SmallChange = 5;
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.TickFrequency = 50;
+            this.trackBar1.Value = 100;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // ChangeColorButton
+            // 
+            this.ChangeColorButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ChangeColorButton.Location = new System.Drawing.Point(414, 63);
+            this.ChangeColorButton.Name = "ChangeColorButton";
+            this.ChangeColorButton.Size = new System.Drawing.Size(117, 23);
+            this.ChangeColorButton.TabIndex = 1;
+            this.ChangeColorButton.Text = "Change color..";
+            this.ChangeColorButton.UseVisualStyleBackColor = true;
+            this.ChangeColorButton.Click += new System.EventHandler(this.ChangeColorButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(455, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 360);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ChangeColorButton);
+            this.Controls.Add(this.trackBar1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer UpdateTimer;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button ChangeColorButton;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Programmeringsgruppen/BestOfLife/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BestOfLife
+{
+    public partial class Form1 : Form
+    {
+        bool[,] matrix;
+
+        int cellSize = 20;
+
+        Color cellColor = Color.DarkOliveGreen;
+
+        SolidBrush brush = new SolidBrush(Color.DarkOliveGreen);
+
+        bool drawing;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            matrix = new bool[20, 20];
+
+            matrix[10, 10] = true;
+            matrix[11, 11] = true;
+            matrix[11, 12] = true;
+            matrix[9, 11] = true;
+            matrix[9, 12] = true;
+            matrix[10, 11] = true;
+            matrix[10, 10] = true;
+        }
+        
+        private void UpdateTimer_Tick(object sender, EventArgs e)
+        {
+            bool[,] nextState = new bool[matrix.GetLength(0), matrix.GetLength(1)];
+
+            for (int x = 0; x < matrix.GetLength(0); x++)
+            {
+                for (int y = 0; y < matrix.GetLength(1); y++)
+                {
+                    int neighbors = NeighborCount(matrix, x, y);
+
+                    if (matrix[x, y] == true)
+                    {
+                        if (neighbors < 2 || neighbors > 3)
+                        {
+                            nextState[x, y] = false;
+                        }
+                        else if (neighbors == 2 || neighbors == 3)
+                        {
+                            nextState[x, y] = true;
+                        }
+                    }
+                    else
+                    {
+                        if (neighbors == 3)
+                        {
+                            nextState[x, y] = true;
+                        }
+                    }
+                }
+            }
+
+            matrix = nextState;
+
+            Invalidate();
+        }
+
+        static int NeighborCount(bool[,] state, int x, int y)
+        {
+            int count = 0;
+
+            bool[,] mask = new bool[3, 3];
+
+            for (int i = 0; i < mask.GetLength(0); i++)
+            {
+                for (int j = 0; j < mask.GetLength(1); j++)
+                {
+                    mask[i, j] = true;
+                }
+            }
+
+            mask[1, 1] = false;
+
+            if (x <= 0)
+            {
+                mask[0, 0] = false;
+                mask[0, 1] = false;
+                mask[0, 2] = false;
+            }
+
+            if (x >= state.GetLength(0) - 1)
+            {
+                mask[2, 0] = false;
+                mask[2, 1] = false;
+                mask[2, 2] = false;
+            }
+
+            if (y <= 0)
+            {
+                mask[0, 0] = false;
+                mask[1, 0] = false;
+                mask[2, 0] = false;
+            }
+
+            if (y >= state.GetLength(1) - 1)
+            {
+                mask[0, 2] = false;
+                mask[1, 2] = false;
+                mask[2, 2] = false;
+            }
+
+            for (int x2 = -1; x2 < 2; x2++)
+            {
+                for (int y2 = -1; y2 < 2; y2++)
+                {
+                    if (mask[x2 + 1, y2 + 1] == true)
+                    {
+                        if (state[x + x2, y + y2] == true)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            for (int x = 0; x < matrix.GetLength(0); x++)
+            {
+                for (int y = 0; y < matrix.GetLength(1); y++)
+                {
+                    if (matrix[x, y] == true)
+                    {
+                        brush.Color = cellColor;
+
+                        g.FillRectangle(brush, x * cellSize, y * cellSize, cellSize, cellSize);
+                    }
+
+                    g.DrawRectangle(Pens.Black, x * cellSize, y * cellSize, cellSize, cellSize);
+                }
+            }
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateTimer.Interval = trackBar1.Value;
+        }
+
+        private void ChangeColorButton_Click(object sender, EventArgs e)
+        {
+            colorDialog1.Color = cellColor;
+
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                cellColor = colorDialog1.Color;
+            }
+
+            Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (UpdateTimer.Enabled)
+            {
+                UpdateTimer.Stop();
+                button1.Text = "Play";
+            }
+            else if (UpdateTimer.Enabled == false)
+            {
+                UpdateTimer.Start();
+                button1.Text = "Pause";
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int matrixX = e.X / matrix.GetLength(0);
+            int matrixY = e.Y / matrix.GetLength(1);
+
+            if (matrixX >= 0 && matrixX < matrix.GetLength(0))
+            {
+                if (matrixY >= 0 && matrixY < matrix.GetLength(1))
+                {
+                    matrix[matrixX, matrixY] = !matrix[matrixX, matrixY];
+                }
+            }
+
+            Invalidate();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                int matrixX = e.X / matrix.GetLength(0);
+                int matrixY = e.Y / matrix.GetLength(1);
+
+                if (matrixX >= 0 && matrixX < matrix.GetLength(0))
+                {
+                    if (matrixY >= 0 && matrixY < matrix.GetLength(1))
+                    {
+                        matrix[matrixX, matrixY] = drawing;
+                    }
+                }
+            }
+
+            Invalidate();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int matrixX = e.X / matrix.GetLength(0);
+            int matrixY = e.Y / matrix.GetLength(1);
+
+            if (matrixX >= 0 && matrixX < matrix.GetLength(0))
+            {
+                if (matrixY >= 0 && matrixY < matrix.GetLength(1))
+                {
+                    drawing = !matrix[matrixX, matrixY];
+                }
+            }
+
+            Invalidate();
+        }
+    }
+}
