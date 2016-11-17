895185bcfd94ba7bf3069daa833f3eaac5e4a438
NogginBops <julius_hager@hotmail.com>
2016-11-17 15:26:45 +0100

    Added BestPad project

---
+++ Programmeringsgruppen/BestPad/MainEditor.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace BestPad
+{
+    partial class MainEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainText = new System.Windows.Forms.TextBox();
+            this.MainMenu = new System.Windows.Forms.MenuStrip();
+            this.FileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.NewFileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenFileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveFileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainText
+            // 
+            this.MainText.AllowDrop = true;
+            this.MainText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainText.Location = new System.Drawing.Point(0, 24);
+            this.MainText.Multiline = true;
+            this.MainText.Name = "MainText";
+            this.MainText.Size = new System.Drawing.Size(422, 288);
+            this.MainText.TabIndex = 0;
+            this.MainText.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainText_DragDrop);
+            this.MainText.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainText_DragEnter);
+            // 
+            // MainMenu
+            // 
+            this.MainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileMenuItem});
+            this.MainMenu.Location = new System.Drawing.Point(0, 0);
+            this.MainMenu.Name = "MainMenu";
+            this.MainMenu.Size = new System.Drawing.Size(422, 24);
+            this.MainMenu.TabIndex = 1;
+            this.MainMenu.Text = "menuStrip1";
+            // 
+            // FileMenuItem
+            // 
+            this.FileMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.NewFileMenuItem,
+            this.OpenFileMenuItem,
+            this.SaveFileMenuItem});
+            this.FileMenuItem.Name = "FileMenuItem";
+            this.FileMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.FileMenuItem.Text = "File";
+            // 
+            // NewFileMenuItem
+            // 
+            this.NewFileMenuItem.Name = "NewFileMenuItem";
+            this.NewFileMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.NewFileMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.NewFileMenuItem.Text = "New";
+            this.NewFileMenuItem.Click += new System.EventHandler(this.NewFileMenuItem_Click);
+            // 
+            // OpenFileMenuItem
+            // 
+            this.OpenFileMenuItem.Name = "OpenFileMenuItem";
+            this.OpenFileMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.OpenFileMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.OpenFileMenuItem.Text = "Open";
+            this.OpenFileMenuItem.Click += new System.EventHandler(this.OpenFileMenuItem_Click);
+            // 
+            // SaveFileMenuItem
+            // 
+            this.SaveFileMenuItem.Name = "SaveFileMenuItem";
+            this.SaveFileMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.SaveFileMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.SaveFileMenuItem.Text = "Save";
+            this.SaveFileMenuItem.Click += new System.EventHandler(this.SaveFileMenuItem_Click);
+            // 
+            // MainEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 312);
+            this.Controls.Add(this.MainText);
+            this.Controls.Add(this.MainMenu);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.MainMenu;
+            this.Name = "MainEditor";
+            this.Text = "BestPad v1.0";
+            this.MainMenu.ResumeLayout(false);
+            this.MainMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox MainText;
+        private System.Windows.Forms.MenuStrip MainMenu;
+        private System.Windows.Forms.ToolStripMenuItem FileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem NewFileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem OpenFileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SaveFileMenuItem;
+    }
+}
+
---
+++ Programmeringsgruppen/BestPad/MainEditor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BestPad
+{
+    public partial class MainEditor : Form
+    {
+        public MainEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void NewFileMenuItem_Click(object sender, EventArgs e)
+        {
+            MainText.Text = "";
+        }
+
+        private void OpenFileMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+            ApplyDefaultDialogSettings(fileDialog);
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                MainText.Text = File.ReadAllText(fileDialog.FileName);
+            }
+        }
+
+        private void SaveFileMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog fileDialog = new SaveFileDialog();
+
+            ApplyDefaultDialogSettings(fileDialog);
+            
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (Stream stream = fileDialog.OpenFile())
+                {
+                    using (StreamWriter writer = new StreamWriter(stream))
+                    {
+                        writer.Write(MainText.Text);
+                    }
+                }
+            }
+        }
+
+        private void ApplyDefaultDialogSettings(FileDialog fileDialog)
+        {
+            fileDialog.AddExtension = true;
+
+            fileDialog.DefaultExt = "txt";
+
+            fileDialog.Title = "Save a text file! :D";
+
+            fileDialog.Filter = "Text|*.txt";
+        }
+        
+        private void MainText_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void MainText_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            MainText.Text = File.ReadAllText(files[0]);
+        }
+    }
+}
