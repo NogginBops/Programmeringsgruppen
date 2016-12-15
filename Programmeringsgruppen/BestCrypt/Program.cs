3ef98d3877c83d7c3b93c9cf2fb422bfe5484ac1
NogginBops <julius_hager@hotmail.com>
2016-12-15 16:39:44 +0100

    Added BestCrypt project

---
+++ Programmeringsgruppen/BestCrypt/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BestCrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start:
+
+            Console.Title = "BestCrypt";
+
+            Console.Write("Encrypt/Decrypt (e/d): ");
+
+            string ed;
+            while ((ed = Console.ReadLine()) != "e" && ed != "d");
+
+            if (ed == "e")
+            {
+                Encrypt();
+            }
+            else
+            {
+                Decrypt();
+            }
+
+            goto start;
+        }
+
+        static void Encrypt()
+        {
+            Console.Write("What file to encrypt?: ");
+
+            string path;
+            while (!File.Exists(path = Console.ReadLine())) Console.WriteLine("Not a file, try again!");
+
+            Console.Write("Encryption key?: ");
+
+            string key = Console.ReadLine();
+
+            // Läs in filen ( File. ... )
+
+            // Ändra värderna med hjälp av nyckeln
+            
+            using (StreamWriter writer = new StreamWriter(File.Create(path + ".best")))
+            {
+                writer.Write("Something! :D");
+            }
+        }
+
+        static void Decrypt()
+        {
+            Console.Write("What file to decrypt?: ");
+
+            string path;
+            while (!File.Exists(path = Console.ReadLine()) && Path.GetExtension(path) == "best") Console.WriteLine("Not a valid file, try again!");
+
+            Console.Write("Decryption key?: ");
+
+            string key = Console.ReadLine();
+
+            // Läs in filen ( File. ... )
+
+            // Ändra värderna med hjälp av nyckeln
+            
+            using (StreamWriter writer = new StreamWriter(File.Create(Path.GetDirectoryName(path) + "/" + Path.GetFileNameWithoutExtension(path))))
+            {
+                writer.Write("Something else! :P");
+            }
+        }
+    }
+}
